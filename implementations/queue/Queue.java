51ae7423592f5d0f3ec57a2dc559e66be3b529f7
KangerDrew <kangdrew95@gmail.com>
2021-11-05 20:25:47 -0700

    Peek n Poll

--- implementations/queue/Queue.java
+++ implementations/queue/Queue.java
@@ -19,5 +19,21 @@ public class Queue <T> implements Iterable <T> {
         return size() == 0;
     }
 
+    // Peek the element at the front of the queue
+    // If queue is empty, throw an error
+
+    public T peek() {
+        if(isEmpty())
+            throw new RuntimeException("Queue Empty");
+        return list.peekFirst();
+    }
+    // Poll an element from the front of the queue
+    // If queue is empty, throw an error
+    public T poll() {
+        if(isEmpty())
+            throw new RuntimeException("Queue Empty");
+        return list.removeFirst();
+    }
+
 
 }
